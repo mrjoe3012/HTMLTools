50e78a0a1d56a6cc70e3190847a1fdd22d5eff0f
mrjoe3012 <josephagrane@gmail.com>
2020-05-28 02:56:06 +0100

    Continued with tag implementation

---
+++ HTMLTools/SVG.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLTools
+{
+    public class SVG : HTMLElement, INest
+    {
+        public List<HTMLElement> NestedElements { get; set; }
+
+        public HTMLFieldAttribute Width { get; set; }
+        public HTMLFieldAttribute Height { get; set; }
+
+        public SVG() : base("svg")
+        {
+            this.Width = new HTMLFieldAttribute("width");
+            this.Height = new HTMLFieldAttribute("height");
+            this.NestedElements = new List<HTMLElement>();
+            this.InitAttributes();
+        }
+
+    }
+}
